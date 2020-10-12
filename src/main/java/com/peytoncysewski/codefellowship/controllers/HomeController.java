b16b30f29faa5119b5a92eb21e8a7712a53c286a
Peyton Cysewski <pcysewski12@gmail.com>
2020-10-11 19:22:58 -0700

    added dynamic nav bar

--- src/main/java/com/peytoncysewski/codefellowship/controllers/HomeController.java
+++ src/main/java/com/peytoncysewski/codefellowship/controllers/HomeController.java
@@ -1,13 +1,32 @@
 package com.peytoncysewski.codefellowship.controllers;
 
+import com.peytoncysewski.codefellowship.models.user.ApplicationUser;
+import com.peytoncysewski.codefellowship.models.user.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
     @GetMapping("/")
-    public String renderHome() {
+    public String renderHome(Principal p, Model m) {
+        if (p == null) {
+            System.out.println("null");
+            m.addAttribute("isSignedIn", false);
+        }
+        else {
+            System.out.println(p.getName());
+            ApplicationUser user = applicationUserRepository.findByUsername(p.getName());
+            m.addAttribute("isSignedIn", true);
+            m.addAttribute("user", user);
+        }
         System.out.println("Rendering the Home page");
         return "home";
     }
