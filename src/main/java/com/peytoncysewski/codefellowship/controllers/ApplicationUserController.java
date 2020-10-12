9484a43a5147eec358498c219e5011323dd3757f
Peyton Cysewski <pcysewski12@gmail.com>
2020-10-11 19:52:40 -0700

    added profile and error pages

--- src/main/java/com/peytoncysewski/codefellowship/controllers/ApplicationUserController.java
+++ src/main/java/com/peytoncysewski/codefellowship/controllers/ApplicationUserController.java
@@ -5,6 +5,9 @@ import com.peytoncysewski.codefellowship.models.user.ApplicationUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -13,7 +16,7 @@ import java.sql.Date;
 @Controller
 public class ApplicationUserController {
     @Autowired
-    ApplicationUserRepository applicationUserRepository;
+    private ApplicationUserRepository applicationUserRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -31,5 +34,14 @@ public class ApplicationUserController {
         ApplicationUser user = new ApplicationUser(username, password, firstName, lastName, dateOfBirth, bio);
         applicationUserRepository.save(user);
         return new RedirectView("/");
+    }
+
+    @GetMapping("/user/{username}")
+    public String renderProfile(@PathVariable String username, Model m) {
+        ApplicationUser user = applicationUserRepository.findByUsername(username);
+        if (user == null)
+            return "error";
+        m.addAttribute("user", user);
+        return "profile";
     }
 }
